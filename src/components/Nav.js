9d61d254c07b28e88df79c1d6994de85795a5415
ygvbhy <ygvbhy@naver.com>
2024-11-25 23:17:09 +0900

    useLocation 을 이용한 검색 페이지 구현

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from "react";
 import "./Nav.css";
+import { useNavigate } from "react-router-dom";
 
 const Nav = () => {
   const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -21,6 +29,14 @@ const Nav = () => {
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
         className="nav__logo"
         onClick={() => window.location.reload()}
+      />
+
+      <input
+        type="text"
+        value={searchValue}
+        onChange={handleChange}
+        className="nav__input"
+        placeholder="영화를 검색해주세요."
       />
 
       <img
--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "../../api/axios";
 
 const SearchPage = () => {
+  const [searchResult, setSearchResult] = useState("");
+
+  // 검색한 내용이 주소창에 실시간 변경이 진행 됨.
+  // 해당 입력 내용을 가져오는 함수
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+
+  let query = useQuery();
+  const searchTerm = query.get("q");
+
+  useEffect(() => {
+    if (searchTerm) fetchSeachMovie(searchTerm);
+  }, [searchTerm]);
+
+  // 검색한 영화 검색
+  const fetchSeachMovie = async (searchTerm) => {
+    try {
+      const request = await axios.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`
+      );
+      setSearchResult(request.data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return <div>SearchPage</div>;
 };
 
