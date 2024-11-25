4756379d55ceb3a6de7e088e0270648836c40ab0
ygvbhy <ygvbhy@naver.com>
2024-11-25 22:56:07 +0900

    Netfilx 앱에 React Router Dom 적용하기

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
 import "./App.css";
 import Nav from "./components/Nav";
-import Banner from "./components/Banner";
-import Row from "./components/Row";
-import requests from "./api/requests";
+import MainPage from "./pages/MainPage";
+import SearchPage from "./pages/SearchPage";
+import DetailPage from "./pages/DetailPage";
 import Footer from "./components/Footer";
+import { Outlet, Routes, Route } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+
+      <Outlet />
+
+      <Footer />
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <div className="app">
-      <Nav></Nav>
-      <Banner></Banner>
-
-      <Row
-        title="NETFLEX ORIGINALS"
-        id="NO"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-      />
-      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
-      <Row
-        title="Action Movies"
-        id="AM"
-        fetchUrl={requests.fetchActionMovies}
-      />
-      <Row
-        title="Comedy Movies"
-        id="CM"
-        fetchUrl={requests.fetchComedyMovies}
-      />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 };
---
+++ src/pages/MainPage/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Banner from "../../components/Banner";
+import Row from "../../components/Row";
+import requests from "../../api/requests";
+
+const MainPage = () => {
+  return (
+    <>
+      <Banner></Banner>
+
+      <Row
+        title="NETFLEX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
+    </>
+  );
+};
+
+export default MainPage;
