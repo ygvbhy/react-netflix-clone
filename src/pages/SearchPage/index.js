4bbb901267c5eec9aebfd20c76c1874efbe34552
ygvbhy <ygvbhy@naver.com>
2024-11-25 23:26:09 +0900

    검색 페이지 UI 구현

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "../../api/axios";
+import "./SearchPage.css";
 
 const SearchPage = () => {
   const [searchResult, setSearchResult] = useState("");
@@ -30,7 +31,37 @@ const SearchPage = () => {
     }
   };
 
-  return <div>SearchPage</div>;
+  const renderSearchResults = () => {
+    return searchResult.length > 0 ? (
+      <section className="search-container">
+        {searchResult.map((movie) => {
+          if (movie.backdrop_path !== null && movie.media_type !== "person") {
+            const movieImageUrl =
+              "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+            return (
+              <div className="movie">
+                <div className="movie__column-poster">
+                  <img
+                    src={movieImageUrl}
+                    alt="이미지"
+                    className="movie__poster"
+                  />
+                </div>
+              </div>
+            );
+          }
+        })}
+      </section>
+    ) : (
+      <section className="no-results">
+        <div className="no-results__text">
+          <p>찾고자하는 검색어 "{searchTerm}"에 맞는 영화가 없습니다.</p>
+        </div>
+      </section>
+    );
+  };
+
+  return renderSearchResults();
 };
 
 export default SearchPage;
